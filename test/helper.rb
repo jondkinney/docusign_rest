53baafb9aa19f87a514b7ceffdd765368049f3cf
Jon Kinney <jonkinney@gmail.com>
2013-08-13 11:01:12 -0500

    Filter sensitive data from VCR cassets

--- test/helper.rb
+++ test/helper.rb
@@ -9,4 +9,9 @@ require 'docusign_login_config'
 VCR.configure do |c|
   c.cassette_library_dir = "test/fixtures/vcr"
   c.hook_into :fakeweb
+
+  c.filter_sensitive_data('%ACCOUNT_ID%') { DocusignRest.account_id }
+  c.filter_sensitive_data('%USERNAME%') { DocusignRest.username }
+  c.filter_sensitive_data('%PASSWORD%') { DocusignRest.password }
+  c.filter_sensitive_data('%INTEGRATOR_KEY%') { DocusignRest.integrator_key }
 end
