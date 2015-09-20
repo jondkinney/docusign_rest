c7215d41af38c491e6acaf0838d79edd50ee41f8
Karney Li <karneyli@gmail.com>
2015-09-20 10:37:46 -0400

    fix bugs in TabsUpdater where we weren't actually updating any tabs

--- lib/models/tabs_updater.rb
+++ lib/models/tabs_updater.rb
@@ -1,47 +1,54 @@
 module Docusign
   class TabsUpdater
-    attr_accessor :envelope_id, :recipient_id, :docusign_client
+    attr_accessor :docusign_client
 
     def initialize(envelope_id, recipient_id)
-      @docusign_client = DocusignRest::Client.new
       @envelope_id = envelope_id
       @recipient_id = recipient_id
-      @lookup = {}
-      retrieve_tabs
+      @docusign_client = DocusignRest::Client.new
+      @tabs = tabs
     end
 
     def set(label, value)
-      @lookup[label].value = value  unless @lookup[label].nil?
+      label = label.to_s
+      @tabs[label].value = value  if @tabs[label].present?
+      @tabs[label]
     end
 
     def [](label)
-      @lookup[label]
+      @tabs[label]
     end
 
     def execute!
-      updates = @lookup.values.
+      updates = @tabs.values.
         select { |tab| tab.dirty? }.
-        group_by { |tab| tab.class.collection_name }
+        group_by { |tab| tab.collection_name }.
+        reduce({}) { |result, (name,tabs)| result[name] = tabs.map(&:to_h); result }
 
-      docusign_client.modify_tabs(envelope_id, recipient_id, updates)  if updates.present?
+      @docusign_client.modify_tabs(@envelope_id, @recipient_id, updates)  if updates.present?
     end
 
   private
 
-    def retrieve_tabs
-      metadata = docusign_client.retrieve_tabs(envelope_id, recipient_id)
-      metadata.each do |collection_type, tabs|
-        tab_class = Tab.class_for(collection_type)
     def class_for(collection_name)
       name = collection_name.to_s.singularize.camelize
       "Docusign::#{name}".constantize
     rescue => e
     end
+
+    def tabs
+      metadata = @docusign_client.retrieve_tabs(@envelope_id, @recipient_id)
+      return {}  unless metadata.present?
+
+      metadata.reduce({}) do |result, (collection_name, tabs)|
+        tab_class = class_for(collection_name)
         tabs.each do |tab|
-          label = tab[:tabLabel]
-          @lookup[label] = tab_class.new(id: tab[:tabId], label: label)
-        end
-      end  unless metadata.nil?
+          label = tab[:tabLabel].to_s
+          result[label] = tab_class.new(id: tab[:tabId], label: label)
+        end  unless tab_class.nil?
+        result
+      end
     end
+
   end
 end
