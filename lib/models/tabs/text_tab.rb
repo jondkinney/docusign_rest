367b593d15fb085e600d6b9a3b51ffd538928cdc
Karney Li <karneyli@gmail.com>
2015-09-29 12:17:56 +0200

    TextTab value saves nil as empty string

--- lib/models/tabs/text_tab.rb
+++ lib/models/tabs/text_tab.rb
@@ -1,7 +1,7 @@
 module Docusign
   class TextTab < Tab
     def to_h
-      super.merge(value: value)
+      super.merge(value: (value.nil? ? '' : value).to_s).compact
     end
   end
 end
--- spec/lib/models/tabs/text_tab_spec.rb
+++ spec/lib/models/tabs/text_tab_spec.rb
@@ -1,14 +1,30 @@
 describe Docusign::TextTab do
 
-  let(:tab) { FactoryGirl.build(:text_tab, value: true) }
+  let(:tab) { FactoryGirl.build(:text_tab, value: value) }
+  let(:value) { 'myValue' }
+  let(:label) { tab.label }
 
   describe "#to_h" do
     subject { tab.to_h }
-    it { is_expected.to eq(tabId: tab.id, tabLabel: tab.label, locked: true, value: tab.value) }
+
+    context "when value is a string" do
+      let(:value) { 'myValue' }
+      it { is_expected.to eq(tabLabel: label, locked: true, value: value ) }
+    end
+
+    context "when value is not a stirng" do
+      let(:value) { 'myValue' }
+      it { is_expected.to eq(tabLabel: label, locked: true, value: value.to_s ) }
+    end
+
+    context "when value is nil" do
+      let(:value) { nil }
+      it { is_expected.to eq(tabLabel: label, locked: true, value: '') }
+    end
   end
 
-  describe ".collection_name" do
-    subject { described_class.collection_name }
+  describe "#collection_name" do
+    subject { tab.collection_name }
     it { is_expected.to eq(:textTabs) }
   end
 end
