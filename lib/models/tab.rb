0bb19bac8a2b3b0bc296f8886359c231ffb411c3
Karney Li <karneyli@gmail.com>
2015-09-29 09:52:20 +0200

    Tab model with .group
    for a Array of objects, if the contents are Tab subclasses groups the
    tabs by the collection name.

--- lib/models/tab.rb
+++ lib/models/tab.rb
@@ -2,13 +2,18 @@ module Docusign
   class Tab
     attr_accessor :id, :label, :value, :dirty
 
-    def initialize(options={})
-      @id = options[:id]
-      @label = options[:label]
+    def initialize(label: nil, value: nil, id: nil)
+      @id = id
+      @label = label
+      @value = value
     end
 
     def to_h
-      { tabId: id, tabLabel: label, locked: true }
+      {
+        tabId: id,
+        tabLabel: label,
+        locked: true
+      }.compact
     end
 
     def value=(value)
@@ -23,6 +28,16 @@ module Docusign
     def collection_name
       self.class.name.demodulize.pluralize.camelize(:lower).to_sym
     end
+
+    def self.group(tabs)
+      return  unless tabs.present?
+      tabs.
+        select { |e| e.is_a?(Tab) }.
+        reject { |e| e.class == Tab }.
+        compact.
+        group_by { |tab| tab.collection_name }
+    end
+
 
   end
 end
--- spec/lib/models/tab_spec.rb
+++ spec/lib/models/tab_spec.rb
@@ -1,31 +1,7 @@
 describe Docusign::Tab do
 
-  describe ".class_for" do
-    subject{ Docusign::Tab.to_class(key) }
-
-    context ":checkboxTabs" do
-      let(:key) { :checkboxTabs }
-      it { is_expected.to eq(Docusign::CheckboxTab) }
-    end
-
-    context ":textTabs" do
-      let(:key) { :textTabs }
-      it { is_expected.to eq(Docusign::TextTab) }
-    end
-
-    context "'checkboxTabs'" do
-      let(:key) { 'checkboxTabs' }
-      it { is_expected.to eq(Docusign::CheckboxTab)}
-    end
-
-    context "'textTabs'" do
-      let(:key) { 'textTabs' }
-      it { is_expected.to eq(Docusign::TextTab)}
-    end
-  end
-
   describe "#value=" do
-    subject(:tab) { described_class.new }
+    subject(:tab) { Docusign::Tab.new(label: 'myLabel') }
 
     its(:dirty?) { is_expected.to be(false) }
     its(:value) { is_expected.to be_nil }
@@ -35,20 +11,63 @@ describe Docusign::Tab do
   end
 
   describe "#to_h" do
-    subject { described_class.new(id: 'abc', label: 'myLabel').to_h }
-    its(:to_h) { is_expected.to eq(tabId: 'abc', tabLabel: 'myLabel', locked: true) }
+    let(:value) { 'myValue' }
+    subject { tab.to_h }
+
+    context "with id" do
+      let(:tab) { Docusign::Tab.new(label: 'myLabel', id: 'abc', value: value) }
+      it { is_expected.to eq(tabLabel: 'myLabel', tabId: 'abc', locked: true) }
+    end
+
+    context "without id" do
+      let(:tab) { Docusign::Tab.new(label: 'myLabel', value: value) }
+      it { is_expected.to eq(tabLabel: 'myLabel', locked: true) }
+    end
   end
 
   describe "#collection_name" do
+    let(:label) { 'myLabel' }
     context Docusign::Tab do
-      it("equals :tabs") { expect(described_class.new.collection_name).to eq(:tabs) }
+      it("equals :tabs") { expect(Docusign::Tab.new(label: label).collection_name).to eq(:tabs) }
     end
     context Docusign::TextTab do
-      it("equals :textTabs") { expect(described_class.new.collection_name).to eq(:textTabs) }
+      it("equals :textTabs") { expect(Docusign::TextTab.new(label: label).collection_name).to eq(:textTabs) }
     end
     context Docusign::CheckboxTab do
-      it("equals :checkboxTabs") { expect(described_class.new.collection_name).to eq(:checkboxTabs) }
+      it("equals :checkboxTabs") { expect(Docusign::CheckboxTab.new(label: label).collection_name).to eq(:checkboxTabs) }
     end
   end
 
+
+  describe ".group" do
+    let(:text_tab1) { FactoryGirl.build(:text_tab, label: 'x') }
+    let(:text_tab2) { FactoryGirl.build(:text_tab, label: 'y') }
+    let(:checkbox_tab1) { FactoryGirl.build(:checkbox_tab, label: 'a') }
+    let(:checkbox_tab2) { FactoryGirl.build(:checkbox_tab, label: 'b') }
+    let(:tab) { FactoryGirl.build(:tab) }
+    let(:tabs) { [text_tab1, text_tab2, checkbox_tab1, checkbox_tab2] }
+
+    subject { Docusign::Tab.group(tabs) }
+
+    its(:keys) { is_expected.to eq([:textTabs, :checkboxTabs]) }
+    its([:textTabs]) { is_expected.to eq([text_tab1, text_tab2]) }
+    its([:checkboxTabs]) { is_expected.to eq([checkbox_tab1, checkbox_tab2]) }
+
+    context "tabs is nil" do
+      let(:tabs) { nil }
+      it { is_expected.to be(nil) }
+    end
+
+    context "tabs is empty" do
+      let(:tabs) { {} }
+      it { is_expected.to be(nil) }
+    end
+
+    context "tabs contains non-Tab subclass elements" do
+      let(:tabs) { [text_tab1, checkbox_tab1, "hello", tab] }
+      its(:keys) { is_expected.to eq([:textTabs, :checkboxTabs]) }
+      its([:textTabs]) { is_expected.to eq([text_tab1]) }
+      its([:checkboxTabs]) { is_expected.to eq([checkbox_tab1]) }
+    end
+  end
 end
