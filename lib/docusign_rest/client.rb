889aa76410e65f9458334d8d6b7ec7ee621811db
Gonzalo <gonzalo@wyeworks.com>
2016-09-15 19:00:58 -0300

    Supports getting envelope's senders view URL

--- lib/docusign_rest/client.rb
+++ lib/docusign_rest/client.rb
@@ -854,6 +854,29 @@ module DocusignRest
       JSON.parse(response.body)
     end
 
+    # Public returns the URL for embedded sending
+    #
+    # envelope_id - the ID of the envelope you wish to use
+    # return_url  - the URL you want the user to be directed to after he or she
+    #               closes the view
+    # headers     - optional hash of headers to merge into the existing
+    #               required headers for a multipart request.
+    #
+    # Returns the URL string for embedded sending
+    def get_sender_view(options = {})
+      content_type = { 'Content-Type' => 'application/json' }
+      content_type.merge(options[:headers]) if options[:headers]
+
+      uri = build_uri("/accounts/#{acct_id}/envelopes/#{options[:envelope_id]}/views/sender")
+
+      http = initialize_net_http_ssl(uri)
+
+      request = Net::HTTP::Post.new(uri.request_uri, headers(content_type))
+      request.body = { returnUrl: options[:return_url] }.to_json
+
+      response = http.request(request)
+      JSON.parse(response.body)
+    end
 
     # Public returns the URL for embedded console
     #
